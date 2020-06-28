960b12fe671a15828f5067e29169a8cf6450bf36
fernandomanuel123 <foi.123@hotmail.com>
2020-06-27 22:24:01 -0500

    Agregar conexión

--- app/src/main/java/com/app/mg/aoe/upc/Activities/ReadQRActvity.java
+++ app/src/main/java/com/app/mg/aoe/upc/Activities/ReadQRActvity.java
@@ -1,21 +1,34 @@
 package com.app.mg.aoe.upc.Activities;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.app.mg.aoe.upc.R;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
 
 public class ReadQRActvity extends AppCompatActivity  {
     Button btnqr;
     TextView tvBarCode;
+    EditText e1,e2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +37,85 @@ public class ReadQRActvity extends AppCompatActivity  {
         btnqr = findViewById(R.id.btnScan);
         tvBarCode = findViewById(R.id.tvScan);
         btnqr.setOnClickListener(mOnClickListener);
+        e1= findViewById(R.id.editText);
+        e2= findViewById(R.id.editText2);
 
+
+        Thread myThread = new Thread(new MyServer());
+        myThread.start();
 
     }
+
+    class MyServer implements Runnable{
+
+        ServerSocket ss;
+        Socket mysocket;
+        DataInputStream dis;
+        String message;
+        Handler handler = new Handler();
+
+        @Override
+        public void run() {
+
+            try {
+                ss = new ServerSocket(9700);
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                    Toast.makeText(getApplicationContext(),"Waiting for client",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                while(true){
+                    mysocket = ss.accept();
+                    dis = new DataInputStream(mysocket.getInputStream());
+                    message = dis.readUTF();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),"message recieve from client: " + message, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
+    public void button_click(View v){
+
+        BackgroundTask b = new BackgroundTask();
+        b.execute(e1.getText().toString(),e2.getText().toString());
+    }
+    public class BackgroundTask extends AsyncTask<String,Void,String>{
+        Socket s;
+        DataOutputStream dos;
+        String ip,message;
+
+        @Override
+        protected String doInBackground(String... params) {
+            ip = params[0];
+            message = params[1];
+
+            try {
+                 s= new Socket(ip,9700);
+                 dos = new DataOutputStream(s.getOutputStream());
+                 dos.writeUTF(message);
+
+                 dos.close();
+                 s.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+
     private  View.OnClickListener mOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -49,6 +138,9 @@ public class ReadQRActvity extends AppCompatActivity  {
                     tvBarCode.setText("error");
                 }
     }
+
+
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
